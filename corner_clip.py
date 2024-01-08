45e84b2b4634a7ebfe8fb4075af2ab71fbb65190
Daniel Haarhoff <daniel@rknt.de>
2024-01-08 20:47:09 +0000

    Finish basic shape

--- corner_clip.py
+++ corner_clip.py
@@ -1,31 +1,27 @@
 import cadquery as cq
 from cq_server.ui import ui, show_object
 
-height = 60.0
-width = 80.0
-thickness = 10.0
-diameter = 22.0
-padding = 12.0
+
+corner_length = 30
+panel_height = 10
+thickness = 4
+frame_width = 8
 
 result = (
-    cq.Workplane("XY")
-    .box(height, width, thickness)
-		.edges("|Z")
-		.fillet(5.0)
-		.faces(">Z or <Z")
-		.edges()
-		.fillet(2)
-    .faces(">Z")
-    .workplane()
-    .hole(diameter)
-    .faces(">Z")
-    .workplane()
-    .rect(height - padding, width - padding, forConstruction=True)
-		.vertices()
-		.cboreHole(2.4, 4.4, 2.1)
-		.faces(">Z")
-		.workplane()
-		.hole(diameter+padding, thickness/2)
+  cq.Workplane()
+	.box(corner_length, corner_length, panel_height+thickness)
+	.faces("<Z")
+	.vertices(">XY")
+	.circle(10)
+	.extrude(panel_height+thickness)
+	.faces("<Z")
+	.vertices("<XY")
+	.rect(corner_length-thickness, corner_length-thickness, centered=[0,0])
+	.cutBlind(panel_height)
+	.faces(">Z")
+	.vertices("<XY")
+	.rect(corner_length-thickness-frame_width, corner_length-thickness-frame_width, centered=[0,0])
+	.cutThruAll()
 )
 
 show_object(result)
