32c20b0b626096145977763d2798c82870b65a4c
Daniel Haarhoff <daniel@rknt.de>
2024-01-13 10:30:14 +0000

    Add 3D printable fillet

--- corner_clip.py
+++ corner_clip.py
@@ -3,6 +3,8 @@ from build123d import *
 from ocp_vscode import *
 from copy import copy
 
+reset_show()
+
 length = 30
 panel_thickness = 10
 edge_width = 10
@@ -12,7 +14,7 @@ screw_diam = 4
 with BuildPart(Plane.XY) as corner_clip:
 	with BuildSketch(Plane.XY) as clip_edges:
 		Rectangle(length, length, align=[Align.MIN, Align.MIN])
-		Circle(screw_diam/2+thickness*2)
+		Circle(screw_diam / 2 + thickness * 2)
 	extrude(amount=panel_thickness + thickness)
 
 	with BuildSketch(Plane.XY) as panel:
@@ -25,10 +27,42 @@ with BuildPart(Plane.XY) as corner_clip:
 			Rectangle(length, length, align=[Align.MIN, Align.MIN])
 	extrude(amount=panel_thickness + thickness, mode=Mode.SUBTRACT)
 
-	with Locations((0,0, panel_thickness, thickness)):
-		CounterBoreHole(radius=(screw_diam+0.6)/2, counter_bore_radius=(7+0.6)/2, counter_bore_depth=thickness)
+	fillet(
+		corner_clip.edges()
+		.filter_by(Axis.Z)
+		.filter_by(lambda e: e.bounding_box().max.Z == panel_thickness + thickness)
+		.sort_by_distance((0, 0, 0))[1:3],
+		radius=10,
+	)
 
-reset_show()
+	fillet(
+		corner_clip.edges()
+		.filter_by(Axis.Z)
+		.filter_by(lambda e: e.bounding_box().max.Z == panel_thickness + thickness),
+		radius=2,
+	)
+
+	chamfer(
+		corner_clip.faces()
+		.sort_by(Axis.Z)[-1]
+		.edges(),
+		1,
+	)
+
+	fillet(
+		corner_clip.edges()
+		.filter_by_position(axis=Axis.Z, minimum=panel_thickness+1, maximum=panel_thickness+thickness-1)
+		.filter_by(Axis.Z, reverse=True),
+		2
+	)
+
+	with Locations((0, 0, panel_thickness, thickness)):
+		CounterBoreHole(
+			radius=(screw_diam + 0.6) / 2,
+			counter_bore_radius=(7 + 0.6) / 2,
+			counter_bore_depth=thickness,
+		)
+
 lcl = copy(locals())
 for i in lcl.keys():
 	if isinstance(lcl[i], (BuildPart, BuildSketch, BuildLine)):
