5b4ecf12cfcb3e060b04aad2877888fdb6a92b3c
Daniel Haarhoff <daniel@rknt.de>
2024-01-13 13:46:34 +0000

    Ensure screw holds a solid layer above the panel

--- corner_clip.py
+++ corner_clip.py
@@ -8,80 +8,82 @@ reset_show()
 length = 30
 panel_thickness = 9
 edge_width = 12
-thickness = 3
+side_thick = 3
+top_thick = 4
+total_height = panel_thickness + top_thick
 screw_diam = 4
+screw_head_diam = 7
+screw_tol = 0.6
+countersink_depth = top_thick - 1.5
 layer = 0.16
 
 with BuildPart(Plane.XY) as corner_clip:
-	with BuildSketch(Plane.XY) as clip_edges:
-		Rectangle(length, length, align=[Align.MIN, Align.MIN])
-		Circle(screw_diam / 2 + thickness * 2)
-	extrude(amount=panel_thickness + thickness)
+    with BuildSketch(Plane.XY) as clip_edges:
+        Rectangle(length, length, align=[Align.MIN, Align.MIN])
+        Circle(screw_diam / 2 + side_thick * 2)
+    extrude(amount=total_height)
 
-	with BuildSketch(Plane.XY) as panel:
-		with Locations((thickness, thickness)):
-			Rectangle(length, length, align=[Align.MIN, Align.MIN])
-	extrude(amount=panel_thickness, mode=Mode.SUBTRACT)
+    with BuildSketch(Plane.XY) as panel:
+        with Locations((side_thick, side_thick)):
+            Rectangle(length, length, align=[Align.MIN, Align.MIN])
+    extrude(amount=panel_thickness, mode=Mode.SUBTRACT)
 
-	with BuildSketch(Plane.XY) as panel_light:
-		with Locations((thickness + edge_width, thickness + edge_width)):
-			Rectangle(length, length, align=[Align.MIN, Align.MIN])
-	extrude(amount=panel_thickness + thickness, mode=Mode.SUBTRACT)
+    with BuildSketch(Plane.XY) as panel_light:
+        with Locations((side_thick + edge_width, side_thick + edge_width)):
+            Rectangle(length, length, align=[Align.MIN, Align.MIN])
+    extrude(amount=total_height, mode=Mode.SUBTRACT)
 
-	fillet(
-		corner_clip.edges()
-		.filter_by(Axis.Z)
-		.filter_by(lambda e: e.bounding_box().max.Z == panel_thickness + thickness)
-		.sort_by_distance((0, 0, 0))[0:2],
-		radius=10,
-	)
+    fillet(
+        corner_clip.edges()
+        .filter_by(Axis.Z)
+        .filter_by(lambda e: e.bounding_box().max.Z == total_height)
+        .sort_by_distance((0, 0, 0))[0:2],
+        radius=10,
+    )
 
-	fillet(
-		corner_clip.edges()
-		.filter_by(Axis.Z)
-		.filter_by(lambda e: e.bounding_box().max.Z == panel_thickness + thickness),
-		radius=2,
-	)
+    fillet(
+        corner_clip.edges()
+        .filter_by(Axis.Z)
+        .filter_by(lambda e: e.bounding_box().max.Z == total_height),
+        radius=2,
+    )
 
-	chamfer(
-		corner_clip.faces().sort_by(Axis.Z)[-1].edges(),
-		1,
-		angle=50
-	)
+    chamfer(corner_clip.faces().sort_by(Axis.Z)[-1].edges(), 1, angle=50)
 
-	fillet(
-		corner_clip.edges()
-		.filter_by_position(
-			axis=Axis.Z,
-			minimum=panel_thickness + 1,
-			maximum=panel_thickness + thickness - 1,
-		)
-		.filter_by(Axis.Z, reverse=True),
-		2,
-	)
+    fillet(
+        corner_clip.edges()
+        .filter_by_position(
+            axis=Axis.Z,
+            minimum=panel_thickness + 1,
+            maximum=total_height - 1,
+        )
+        .filter_by(Axis.Z, reverse=True),
+        2,
+    )
 
-	with Locations((0, 0, panel_thickness + thickness)):
-		Hole(
-			radius=(7 + 0.6) / 2,
-			depth=thickness,
-		)
-	with Locations((0, 0, panel_thickness)):
-		Hole(
-			radius=(screw_diam + 0.6) / 2,
-		)
-	with BuildSketch(Plane((0, 0, panel_thickness))):
-		Rectangle(screw_diam+0.6, screw_diam+0.6)
-	extrude(amount=-layer*3, mode=Mode.SUBTRACT)
-	with BuildSketch(Plane((0, 0, panel_thickness))):
-		Circle((7+0.6)/2)
-		Rectangle(screw_diam+0.6, 8, mode=Mode.INTERSECT)
-	extrude(amount=-layer*2, mode=Mode.SUBTRACT)
-
+    with Locations((0, 0, total_height)):
+        Hole(
+            radius=(screw_head_diam + screw_tol) / 2,
+            depth=countersink_depth,
+        )
+    with Locations((0, 0, total_height)):
+        Hole(
+            radius=(screw_diam + screw_tol) / 2,
+        )
+    with BuildSketch(Plane((0, 0, total_height - countersink_depth))):
+        Rectangle(screw_diam + screw_tol, screw_diam + screw_tol)
+    extrude(amount=-layer * 3, mode=Mode.SUBTRACT)
+    with BuildSketch(Plane((0, 0, total_height - countersink_depth))):
+        Circle((screw_head_diam + screw_tol) / 2),
+        Rectangle(
+            screw_diam + screw_tol, screw_head_diam + screw_tol, mode=Mode.INTERSECT
+        )
+    extrude(amount=-layer * 2, mode=Mode.SUBTRACT)
 
 lcl = copy(locals())
 for i in lcl.keys():
-	if isinstance(lcl[i], (BuildPart, BuildSketch, BuildLine)):
-		show_object(lcl[i])
+    if isinstance(lcl[i], (BuildPart, BuildSketch, BuildLine)):
+        show_object(lcl[i])
 
 
 # %%
